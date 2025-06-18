ffc7617e9800ccfdcd46d0d8b829fa7598a7af38
Ever VC <ever.vasquezc27@gmail.com>
2025-06-17 20:16:36 -0600

    ✨ New Feature: Permite actualizar los campos en tiempo real, puede servir para un buscador

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,6 +1,6 @@
 <div>
     {{-- <h2>{{ $title }} desde el componente! Usuario:[{{ $name }}, {{$email}}] </h2> --}}
-    <x-input type="text" wire:model="name" placeholder="Escribe el título del post" />
+    <x-input type="text" wire:model.live="name" placeholder="Escribe el título del post" />
     <x-button wire:click="save">
         Save
     </x-button>
