63c50af3591e9aa5a3b47deb90eb97a0abe9bc69
Ever VC <ever.vasquezc27@gmail.com>
2025-06-17 19:38:39 -0600

    💥 NEW: Creando variables para mostrar información en la vista desde la clase del componente

--- app/Livewire/CreatePost.php
+++ app/Livewire/CreatePost.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class CreatePost extends Component
 {
+    // Permite almacenar lo siguiente:
+    // Arreglos, Strings, Enteros, Flotantes, Booleanos, Objetos, Colecciones, Null etc.
+
+    public $title = 'Hola Mundo'; // Se puede acceder desde la vista con $title
     public function render()
     {
         return view('livewire.create-post');
