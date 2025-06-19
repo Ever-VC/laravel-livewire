bb6f96b2264433f9b4a2935eac8d6fe9ec9fb439
Ever VC <ever.vasquezc27@gmail.com>
2025-06-18 18:55:49 -0600

    ✨ New Feature: Contador a través de eventos

---
+++ app/Livewire/Contador.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Contador extends Component
+{
+    public $count = 0;
+    public function render()
+    {
+        return view('livewire.contador');
+    }
+
+    public function restar()
+    {
+        $this->count--;
+    }
+    public function sumar()
+    {
+        $this->count++;
+    }
+}
---
+++ resources/views/livewire/contador.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <x-button wire:click='restar'>-</x-button>
+    <span class="mx-2">{{ $count }}</span>
+    <x-button wire:click='sumar'>+</x-button>
+</div>
