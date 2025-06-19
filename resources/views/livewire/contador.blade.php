5eaf27f1f91646323e2cea977dabee7f5718068e
Ever VC <ever.vasquezc27@gmail.com>
2025-06-18 18:58:40 -0600

    💥 NEW: Parámetro  con el valor que irá aumentando

--- resources/views/livewire/contador.blade.php
+++ resources/views/livewire/contador.blade.php
@@ -1,5 +1,5 @@
 <div>
     <x-button wire:click='restar'>-</x-button>
     <span class="mx-2">{{ $count }}</span>
-    <x-button wire:click='sumar'>+</x-button>
+    <x-button wire:click='sumar(2)'>+</x-button>
 </div>
